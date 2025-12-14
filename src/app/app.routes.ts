2b71aff9d4e54f773eafee6a41df420934dbf38a
Trey Morris <trey@morris2morris.com>
2025-12-14 15:59:01 -0500

    Remove some components, going to do a full refresh soon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' }, 
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'about', component: AboutComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
-import { NavBarComponent } from '../nav-bar/nav-bar.component';
 @Component({
     selector: 'app-home',
-    imports: [RouterOutlet, RouterModule, NavBarComponent],
+    imports: [RouterModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
