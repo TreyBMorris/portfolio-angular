f46db0e10a25752237f3e38b788dde765a1c3a5e
Trey Morris <trey@morris2morris.com>
2025-12-14 15:00:40 -0500

    Upgrate to v21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 @Component({
     selector: 'app-root',
-    imports: [CommonModule, RouterOutlet, RouterModule],
+    imports: [RouterOutlet, RouterModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
